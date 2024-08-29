703a21a9ebb2ca4220ab4f930005098ca0763178
Julian MONTAGNE <julian.montagne2023@campus-eni.fr>
2024-08-29 11:22:14 +0200

    feat: Produits

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\ProduitRepository;
 use App\Repository\BouquetRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -10,7 +11,7 @@ use Symfony\Component\Routing\Attribute\Route;
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(BouquetRepository $bouquetRepo): Response
+    public function index(ProduitRepository $produitRep, BouquetRepository $bouquetRepo): Response
     {
         $fakeReplicas = $bouquetRepo->findAll();
         $randomfakeReplicas = $fakeReplicas[array_rand($fakeReplicas)]->getFakeReplica();
@@ -18,6 +19,7 @@ class HomeController extends AbstractController
         return $this->render('home/index.html.twig', [
             'title' => 'Home',
             'fakeReplica' => $randomfakeReplicas,
+            'produits' => $produitRep->findAll(),
         ]);
     }
 }
--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,15 +2,94 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Produit;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\Filesystem\Filesystem;
 
 class AppFixtures extends Fixture
 {
+    public function __construct(private Filesystem $filesystem)
+    {
+    }
+
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $produits = [
+            [
+                'nom' => 'Caleçon sale JCVD',
+                'prix' => 650,
+                'description' => 'Porté lors de sa fameuse cascade dans son film "last action hero"',
+                'image' => 'caleconJCVD.jpg'
+            ],
+            [
+                'nom' => 'Kickboxer : La chaussette anti-transpiration JCVD',
+                'prix' => 99,
+                'description' => 'Pour garder les pieds au sec pendant vos combats de rue',
+                'image' => ''
+            ],
+            [
+                'nom' => 'Universal Soldier : Le camouflage urbain',
+                'prix' => 330,
+                'description' => 'Pour passer inaperçu en pleine ville',
+                'image' => '...'
+            ],
+            [
+                'nom' => "Timecop : Le déodorant 'Retour vers le futur'",
+                'prix' => 45,
+                'description' => 'Pour sentir bon, même après avoir voyagé dans le temps',
+                'image' => ''
+            ],
+            [
+                'nom' => 'Le split de JCVD : Le tapis de yoga pour débutant',
+                'prix' => 120,
+                'description' => 'Pour atteindre la flexibilité légendaire de JCVD',
+                'image' => ''
+            ],
+            [
+                'nom' => "La potion magique de JCVD : Le booster d'énergie",
+                'prix' => 19,
+                'description' => "Pour avoir la même énergie qu'un bourreau des cœurs",
+                'image' => ''
+            ],
+            [
+                'nom' => 'Les lunettes de soleil miroir de JCVD : Pour un regard de killer',
+                'prix' => 220,
+                'description' => 'Pour afficher un style inimitable',
+                'image' => ''
+            ],
+            [
+                'nom' => 'La perruque authentique de Jean Claude',
+                'prix' => 1500,
+                'description' => 'Pour devenir la star de votre soirée',
+                'image' => ''
+            ],
+            [
+                'nom' => 'Le sac de sport "Muscles en béton armé"',
+                'prix' => 100,
+                'description' => "Pour transporter votre matériel d'entraînement avec style",
+                'image' => ''
+            ],
+            [
+                'nom' => "Le livre de recettes 'Cuisine à la JCVD' : Des plats aussi musclés que lui",
+                'prix' => 60,
+                'description' => "Pour préparer des repas dignes d'un champion",
+                'image' => ''
+            ],
+
+        ];
+
+        foreach ($produits as $produitData) {
+            $produit = new Produit();
+            $produit->setNom($produitData['nom'])
+                ->setDescription($produitData['description'])
+                ->setPrix($produitData['prix']);
+            $imagePath = $produitData['image'];
+
+            $produit->setImage($imagePath);
+
+            $manager->persist($produit);
+        }
 
         $manager->flush();
     }
