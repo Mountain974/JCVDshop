02a5711979f91b49829be08ea3f82b96042da99d
Sandie79 <guerin-sandie@hotmail.fr>
2024-08-29 12:11:59 +0200

    feat: fusion

--- src/Controller/FerrailleController.php
+++ src/Controller/FerrailleController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\Ferraille;
 use App\Repository\FerrailleRepository;
+use App\Repository\BouquetRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,11 +12,14 @@ use Symfony\Component\Routing\Annotation\Route;
 class FerrailleController extends AbstractController
 {
     #[Route('/joujouxJCVD', name: 'app_ferraille')]
-    public function index(FerrailleRepository $ferrailleRep): Response
+    public function index(FerrailleRepository $ferrailleRep, BouquetRepository $bouquetRepo): Response
     {
+        $fakeReplicas = $bouquetRepo->findAll();
+        $randomfakeReplicas = $fakeReplicas[array_rand($fakeReplicas)]->getFakeReplica();
         // Renvoyer le template Twig avec les données
         return $this->render('ferraille/index.html.twig', [
-            'controller_name' => 'FerrailleController',
+            'title' => 'Joujoux',
+            'fakeReplica' => $randomfakeReplicas,
             'ferrailles' => $ferrailleRep->findAll(),
         ]);
     }
